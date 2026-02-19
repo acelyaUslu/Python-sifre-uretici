749f407b6115cc1699ae5cfa0d8b6d95b5f2e7e5
Rafiq <rafiqquliyev2023@gmail.com>
2026-02-19 22:38:20 +0400

    Değişikliklerin açıklaması

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -8,10 +8,14 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
-"""
-Aşağıda önerilenlerden birini kullanarak başka bir test yazın. Alternatif olarak, kendi testinizi de oluşturabilirsiniz!
-Daha fazla test yazabilirseniz harika olur!
+def test_password_length():
+    """Şifrenin uzunluğunun belirtilen uzunlukla eşleştiğini test etme"""
+    for length in range(1, 21): 
+        assert len(generate_password(length)) == length
 
-1. Şifrenin uzunluğunun belirtilen uzunlukla eşleşip eşleşmediğini test edin  
-2. Arka arkaya oluşturulan iki şifrenin farklı olup olmadığını test edin 
-"""
+
+def test_password_randomness():
+    """Arka arkaya oluşturulan iki şifrenin farklı olduğunu test etme"""
+    password1 = generate_password(10)
+    password2 = generate_password(10)
+    assert password1 != password2, "Arka arkaya oluşturulan iki şifre aynı olmamalıdır."
